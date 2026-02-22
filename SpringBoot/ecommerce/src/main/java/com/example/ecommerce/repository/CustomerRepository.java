3d27ed0b261f0eaaf8a26b1d890b082bd8811a94
Aparna S <aparnasuresh2417@gmail.com>
2026-02-23 00:29:50 +0530

    Delete SpringBoot/ecommerce directory

--- SpringBoot/ecommerce/src/main/java/com/example/ecommerce/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.ecommerce.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- SpringBoot/ecommerce/src/main/java/com/example/ecommerce/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommerce.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.ecommerce.entity.Payment;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
