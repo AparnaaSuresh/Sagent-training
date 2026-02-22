52f32cdd31f31dc8521fe9f9064093bf0c71e998
Aparna S <aparnasuresh2417@gmail.com>
2026-02-22 22:47:06 +0530

    Delete SpringBoot/LibraryManagement directory

--- SpringBoot/LibraryManagement/src/main/java/com/library/librarymanagement/repository/InventoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.library.librarymanagement.repository;
-
-import com.library.librarymanagement.entity.Inventory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InventoryRepository extends JpaRepository<Inventory, Long> {
-}
